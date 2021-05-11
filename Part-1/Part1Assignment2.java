ddb8323fe6c01802abb92bc8636bb00001dac477
DavisHill-SS <83970776+DavisHill-SS@users.noreply.github.com>
2021-05-11 12:15:46 -0500

    Create Part1Assignment2.java

---
+++ Part-1/Part1Assignment2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Part1Assignment2 {
+
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+		Random rand = new Random();
+		int random = rand.nextInt(100)+1;
+		int guess;
+		int count = 0;
+		
+		while(count < 5) {
+			System.out.print("Enter a number: ");
+			guess = userInput.nextInt();
+			if(Math.abs(guess - random) <= 10) {
+				System.out.println("You have guessed close enough, the number was " + random);
+				break;
+			}
+			else {
+				count += 1;
+				if (count == 5) {
+					System.out.println("Sorry, the number was " + random);
+				}
+				else{
+					System.out.println("Try again");
+				}
+			}
+		}
+		userInput.close();
+	}
+
+}
